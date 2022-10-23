977c3d4424bec6f6264a1408ac4042d8cb4cc71f
Taggagii <jdietrich0603@gmail.com>
2022-10-22 21:29:42 -0400

    adding some new examples

--- helloworld.asm
+++ helloworld.asm
@@ -1,3 +1,7 @@
+section	.data
+msg db 'Hello, world!', 0xa  ; string to be printed
+len equ $ - msg     ; length of the string
+
 section	.text
    global _start     ; must be declared for linker (ld)
 	
@@ -10,7 +14,3 @@ _start:	            ; tells linker entry point
 	
    mov	eax,1       ; system call number (sys_exit)
    int	0x80        ; call kernel
-
-section	.data
-msg db 'Hello, world!', 0xa  ; string to be printed
-len equ $ - msg     ; length of the string
